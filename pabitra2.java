c643d2432fd95a9c23d7c8af79965e5b3bcabbed
pabitra8240016853 <pabitramukherjee_abijit@srmuniv.edu.in>
2017-07-09 14:36:36 +0530

    Add files via upload

---
+++ pabitra2.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			 int y= 6;
+	 if(y%2 == 0)
+	 System.out.println("even");
+	 else 
+	 System.out.println("odd");
+ 
+ 
+ 
